33f1e3ee3f00548a70f53d1e405f5a29821f35c3
Emilin de Souza <emilinadamdalton@gmail.com>
2022-05-15 01:58:14 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,33 @@
+import Pagination from "components/Pagination";
+import MovieCard from "components/MovieCard";
+
 function Listing() {
 
     return (
-        <h1>Página de listagem</h1>
-    );
-    }
+        <>
+            <Pagination />
 
-    export default Listing;
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div> 
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>            
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Listing;
