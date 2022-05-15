cade3d472cdea1ec61f2c3633ccb1b4339cb7cca
Emilin de Souza <emilinadamdalton@gmail.com>
2022-05-15 15:59:11 -0300

    Domain model, data base seed

--- backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
@@ -1,5 +1,39 @@
 package com.devsuperior.dsmovie.config;
 
-public class SecurityConfig {
+import java.util.Arrays;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration 
+@EnableWebSecurity 
+public  class  SecurityConfig  import  WebSecurityConfigurerAdapter {
+
+	@ Ambiente privado autowired 
+	env ;  
+
+	@ Override 
+	protected  void  configure ( HttpSecurity  http ) lança  Exception {
+		 if ( Arrays . asList ( env . getActiveProfiles ()) contém ( "test" )) {
+			 http . cabeçalhos (). frameOptions (). desabilitar ();
+		}
+		
+		http . cora (). e (). csrf (). desabilitar ();
+		http . SessionManagement (). sessionCreationPolicy ( SessionCreationPolicy . STATELESS );
+		http . autorizeRequests (). qualquerRequest (). permitirTodos ();
+	}
+
+	@Bean 
+	CorsConfigurationSource  corsConfigurationSource () {
+		 configuração CorsConfiguration  = new CorsConfiguration (). applyPermitDefaultValues ();
+		configuração . setAllowedMethods ( Arrays . asList ( "POST" , "GET" , "PUT" , "DELETE" , "OPTIONS" ));
+		final UrlBasedCorsConfigurationSource fonte = new UrlBasedCorsConfigurationSource ();
+		fonte .    registerCorsConfiguration ( "/**" , configuração );
+		fonte de retorno  ;
+	}
 }
